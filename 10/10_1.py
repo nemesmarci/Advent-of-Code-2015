6692d20c5118776ec23cfe66d9b1798438318da4
Marcell Nemes <nemes@sch.bme.hu>
2018-12-08 20:12:41 +0100

    Day 10: Elves Look, Elves Say - part 1

---
+++ 10/10_1.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import groupby
+
+num = sys.stdin.readline().strip()
+
+def look_and_say(num):
+    ret = ""
+    for _, g in groupby(num):
+        group = list(g)
+        ret += str(len(group)) + group[0]
+    return ret
+
+for i in range(40):    
+    num = look_and_say(num)
+
+print(len(num))
