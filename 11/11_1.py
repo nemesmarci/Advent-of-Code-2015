01be3be2757711c61681a69b4698515c67c9636f
Marcell Nemes <nemes@sch.bme.hu>
2018-12-16 14:36:40 +0100

    Day 11: Corporate Policy - part 1

---
+++ 11/11_1.py
@@ -0,0 +1,40 @@
+import sys
+from itertools import groupby
+
+password = sys.stdin.readline().strip()
+
+
+def increment(password):
+    last = password[-1]
+    if last == 'z':
+        last = 'a'
+        password = increment(password[:-1]) + last
+    else:
+        last = chr(ord(last) + 1)
+        password = password[:-1] + last
+    return password
+
+triples = [chr(c) + chr(c + 1) + chr(c + 2) for c in range(ord('a'), ord('x') + 1)]
+
+
+def correct(password):
+    if not any(triple in password for triple in triples):
+        return False
+    if any(c in password for c in 'iol'):
+        return False
+    doubles = set()
+    for c, g in groupby(password):
+        if len(list(g)) > 1:
+            doubles.add(c)
+    if len(doubles) < 2:
+        return False
+    return True
+
+password = increment(password)
+
+while not correct(password):
+    password = increment(password)
+
+password = increment(password)
+
+print(password)
