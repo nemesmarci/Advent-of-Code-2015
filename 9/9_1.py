7f578ce9beb3ab03c6f8dd42b770649b4e118405
Marcell Nemes <nemes@sch.bme.hu>
2020-01-08 20:54:19 +0100

    Refactor day 9

--- 9/9_1.py
+++ 9/9_1.py
@@ -1,28 +1,3 @@
-import sys
-from collections import defaultdict
+from common import read_data, traverse
 
-cities = defaultdict(dict)
-shortest = None
-begin = None
-
-for line in sys.stdin.readlines():
-    start, rem = line.split(' to ')
-    end, dist = rem.split(' = ')
-    dist = int(dist)
-    if shortest is None or dist < shortest:
-        shortest = dist
-        begin = start
-    cities[start][end] = dist
-    cities[end][start] = dist
-
-visited = [begin]
-total_distance = 0
-
-while len(visited) < len(cities):
-    valid_targets = {c: d for c, d in cities[begin].items() if c not in visited}
-    end = min(valid_targets.keys(), key=lambda c: valid_targets[c])
-    visited.append(end)
-    total_distance += valid_targets[end]
-    begin = end
-
-print(total_distance)
+print(traverse(*read_data()))
