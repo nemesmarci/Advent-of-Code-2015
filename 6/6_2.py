9a508e87ca9513fbaed723ba97cbd2da2a951c60
Marcell Nemes <nemes@sch.bme.hu>
2020-01-08 17:59:58 +0100

    Refactor day 6

--- 6/6_2.py
+++ 6/6_2.py
@@ -1,20 +1,3 @@
-import re
-import sys
+from common import solve
 
-regex = re.compile(r"([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)")
-
-lights = [[0 for x in range(1000)] for y in range(1000)]
-
-for line in sys.stdin.readlines():
-    action = "on" if "on" in line else "off" if "off" in line else "toggle"
-    a, b, x, y = [int(num) for num in regex.search(line).groups()]
-    for i in range(a, a + x - a + 1):
-        for j in range(b, b + y - b + 1):
-            if action == "on":
-                lights[i][j] += 1
-            elif action == "off":
-                lights[i][j] -= 1 if lights[i][j] > 0 else 0
-            else:
-                lights[i][j] += 2
-
-print(sum([l for ll in lights for l in ll]))
+print(solve(part_two=True))
