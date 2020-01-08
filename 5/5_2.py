700136d6fdba16cc24f3487a1252ef0500cd2dde
Marcell Nemes <nemes@sch.bme.hu>
2020-01-08 17:28:42 +0100

    Refactor day 5

--- 5/5_2.py
+++ 5/5_2.py
@@ -1,24 +1,10 @@
-import sys
+from common import count_nice
+
 
 def is_nice(line):
-    double = False
-    for i in range(len(line) - 1):
-        pair = line[i:i + 2]
-        if pair in line[i + 2:]:
-            double = True
-            break
-    if not double:
-        return 0
-    one_between = False
-    for i in range(len(line) - 2):
-        if line[i] == line[i + 2]:
-            one_between = True
-            break
-    if not one_between:
-        return 0
-    return 1
+    return (
+        any(line[i:i + 2] in line[i + 2:] for i in range(len(line) - 1)) and
+        any(line[i] == line[i + 2] for i in range(len(line) - 2)))
 
-nices = 0
-for line in sys.stdin.readlines():
-    nices += is_nice(line)
-print(nices)
+
+print(count_nice(is_nice))
