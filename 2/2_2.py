845820b1b07a530b8f2f3262b92260655e04c89b
Marcell Nemes <nemes@sch.bme.hu>
2020-01-08 01:22:52 +0100

    Refactor day 2

--- 2/2_2.py
+++ 2/2_2.py
@@ -1,7 +1,3 @@
-import sys
+from common import boxes
 
-sum_length = 0
-for line in sys.stdin.readlines():
-    l, w, h = sorted([int(side) for side in line.split('x')])
-    sum_length += 2 * (l + w) + l * w * h
-print(sum_length)
+print(sum(2 * (l + w) + l * w * h for l, w, h in boxes()))
